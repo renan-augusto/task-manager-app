86b741532a1416a1c6aad7fd0d544ab1a6678eeb
renan-augusto <renan_acm1992@hotmail.com>
2024-04-17 23:17:36 -0300

    feat(front-end) - load componente implemented

---
+++ task-manager-ui/src/app/shared/load/load.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { LoaderService } from '../../core/loader.service';
+import { PoLoadingModule } from '@po-ui/ng-components';
+
+
+@Component({
+  selector: 'app-load',
+  standalone: true,
+  imports: [
+    CommonModule,
+    PoLoadingModule,
+  ],
+  templateUrl: './load.component.html',
+  styleUrl: './load.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class LoadComponent {
+
+  constructor(public loader: LoaderService) {}
+
+}
