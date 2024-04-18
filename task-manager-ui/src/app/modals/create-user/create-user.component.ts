f5a7f9da687107ebadc23f09a629acfa94b50631
renan-augusto <renan_acm1992@hotmail.com>
2024-04-17 23:16:50 -0300

    feat(frontEnd) - create user implemented

--- task-manager-ui/src/app/modals/create-user/create-user.component.ts
+++ task-manager-ui/src/app/modals/create-user/create-user.component.ts
@@ -1,9 +1,11 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output, signal } from '@angular/core';
-import { PoButtonModule, PoDividerModule, PoDynamicFormField, PoDynamicModule, PoFieldModule } from '@po-ui/ng-components';
-import { DynamicFormFieldsService } from '../../core/dynamic-form-fields.service';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { PoButtonModule, PoDividerModule, PoDynamicFormField, PoDynamicModule, PoFieldModule, PoLoadingModule } from '@po-ui/ng-components';
 import { AbstractControl, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../core/auth.service';
 import { UserRequest } from '../../models/user-request.interface';
+import { CommonModule } from '@angular/common';
+import { LoadComponent } from '../../shared/load/load.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @Component({
   selector: 'create-user',
@@ -14,24 +16,26 @@ import { UserRequest } from '../../models/user-request.interface';
     ReactiveFormsModule, 
     PoButtonModule,
     PoDividerModule,
-    PoFieldModule
+    PoFieldModule,
+    PoLoadingModule,
+    CommonModule,
+    LoadComponent
   ],
   templateUrl: './create-user.component.html',
-  styleUrl: './create-user.component.scss'
+  styleUrl: './create-user.component.scss',
 })
 export class CreateUserComponent implements OnInit, OnDestroy {
   
-  @Output() formResult = new EventEmitter<NgForm>();
+  @Output() onFormSucccess = new EventEmitter<boolean>();
 
   fields: PoDynamicFormField[] = [];
   formResponse!: NgForm;
-  registerUserForm!: FormGroup;
-  loading = signal(false);
+  registerUserForm: FormGroup;
+  loading: boolean = false;
   
   
   constructor(
-    private _formService: DynamicFormFieldsService,
-    private _auth: AuthService
+    private _authService: AuthService
   ) {
     this.registerUserForm = new FormGroup(
       {
@@ -48,17 +52,26 @@ export class CreateUserComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.fields = this._formService.getDynamicFormFieldUsers();
-  }
-
-  onFormChange(event: NgForm) {
-    this.formResponse = event;
   }
 
   onFormSubmit() {
-    this.loading.set(true)
+    this.loading = true;
     if(this.registerUserForm.valid) {
       const payload: UserRequest = this.createUserRequest();
+
+      this._authService.userSingup(payload).subscribe({
+        next: () => {
+          this.registerUserForm.reset();
+          this.loading = false;
+          this.onFormSucccess.emit(true);
+        },
+        error: (error) => {
+          console.error('Error submiting the form', error);
+          this.loading = false;
+        }
+      });
+    } else {
+      this.loading = false;
     }
     
   }
@@ -71,10 +84,10 @@ export class CreateUserComponent implements OnInit, OnDestroy {
 
   createUserRequest(): UserRequest {
     return {
-      email: this.registerUserForm.get('email')?.value,
-      password: this.registerUserForm.get('password')?.value,
-      firstName: this.registerUserForm.get('name')?.value,
-      lastName: this.registerUserForm.get('lastName')?.value
+      email: this.registerUserForm.get('email')?.value.trim(),
+      password: this.registerUserForm.get('password')?.value.trim(),
+      firstName: this.registerUserForm.get('name')?.value.trim(),
+      lastName: this.registerUserForm.get('lastName')?.value.trim()
     };
   }
 
