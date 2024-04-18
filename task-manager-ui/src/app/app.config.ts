9702827aae1778c4adbbd99087e85966e2f0f96a
renan-augusto <renan_acm1992@hotmail.com>
2024-04-17 23:17:16 -0300

    chore(frontEnd) - interceptor configured

--- task-manager-ui/src/app/app.config.ts
+++ task-manager-ui/src/app/app.config.ts
@@ -3,15 +3,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PoHttpRequestModule } from '@po-ui/ng-components';
+import { loaderInterceptor } from './interceptors/loader.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule]),
+    provideHttpClient(withInterceptors([loaderInterceptor])),
   ],
   
 };
