51c0d098170f25717a0a612cf48eb57b78fa38da
renan-augusto <renan_acm1992@hotmail.com>
2024-04-19 07:16:32 -0300

    feat(frontend) - Load screen by interceptor

--- task-manager-ui/src/app/app.component.ts
+++ task-manager-ui/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadComponent } from './shared/load/load.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LoadComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
