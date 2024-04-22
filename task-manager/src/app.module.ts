0f111220588ceb7e586ebaf964a11436012849cf
renan-augusto <renan_acm1992@hotmail.com>
2024-04-21 21:47:11 -0300

    chore(back-end) : implemented the menu module at app.module

--- task-manager/src/app.module.ts
+++ task-manager/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
   }),
   AuthModule,
   PrismaModule,
+  MenuModule,
 ],
 })
 export class AppModule {}
