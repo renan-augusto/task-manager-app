a2b6a82524de8db5359f7be9e6cf20945be87a81
renan-augusto <renan_acm1992@hotmail.com>
2024-04-25 06:51:28 -0300

    constructioN(back-end) - task construction

---
+++ task-manager/src/task/dto/task.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
+export class TaskDto {
+    
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty()
+    title: string;
+
+    @IsString()
+    @ApiProperty()
+    description: string;
+
+    @IsBoolean()
+    @ApiProperty()
+    completed: boolean;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    userId: number;
+}
