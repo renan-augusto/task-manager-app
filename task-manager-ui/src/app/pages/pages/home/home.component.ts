d175c88b41c2892ba68af8cb0eb5c4a71f9709f5
renan-augusto <renan_acm1992@hotmail.com>
2024-05-13 07:14:59 -0300

    construction(tasks-charts) - tasks charts construction started

--- task-manager-ui/src/app/pages/pages/home/home.component.ts
+++ task-manager-ui/src/app/pages/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PoChartModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    PoChartModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
